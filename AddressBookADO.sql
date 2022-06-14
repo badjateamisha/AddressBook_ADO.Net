bc6c437ae56cf55ace8a4b59a546cbf49c5f9363
badjateamisha <amishabadjate175@gmail.com>
2022-06-14 22:20:53 +0530

    Get contacts in alphabetically sorted order

--- AddressBookADO.sql
+++ AddressBookADO.sql
@@ -3,3 +3,4 @@ select * from AddressBook
 
 Insert into AddressBook values ('Aarvik','Kalburgi','XYZ Colony','Pune','MH','431001','9000000002','aarvik@gmail.com')
 Insert into AddressBook values ('Sagar','Kalburgi','XYZ Colony','Pune','MH','411033','9000000003','sagar@gmail.com')
+Insert into AddressBook values ('Disha','Badjate','ABC Colony','Abad','MH','431001','9000000004','disha@gmail.com')
